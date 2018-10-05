9bece2a91b0c9feaa61f4ad2c8a6e15c6a9ed2ef
yyuutta <yyuutta@gmail.com>
2018-10-05 03:29:40 +0000

    login and page

---
+++ microposts/app/Http/Controllers/UsersController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\User;
+
+class UsersController extends Controller
+{
+    public function index()
+    {
+        $users = User::paginate(10);
+
+        return view('users.index', [
+            'users' => $users,
+        ]);
+    }
+    
+    public function show($id)
+    {
+        $user = User::find($id);
+
+        return view('users.show', [
+            'user' => $user,
+        ]);
+    }
+    
+}
